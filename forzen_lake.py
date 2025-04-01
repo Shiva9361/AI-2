04ffea5268c63d457f9b5bb589dcb600e9488e2c
Shiva9361 <shivadharshansankar936@gmail.com>
2025-04-01 14:53:46 +0530

    feat: ida* with manhatten huristic

--- forzen_lake.py
+++ forzen_lake.py
@@ -3,7 +3,7 @@ from gymnasium import Env
 from gymnasium.spaces import Discrete
 from gymnasium.envs.toy_text.frozen_lake import generate_random_map
 
-from typing import cast, Tuple
+from typing import cast, Tuple, Callable
 from collections import defaultdict
 
 import imageio
@@ -14,7 +14,7 @@ import time
 import matplotlib.pyplot as plt
 
 N = 8
-Timeout = 10  # in seconds :)
+Timeout = 60  # in seconds :)
 
 env: Env[Discrete, Discrete] = gym.make('FrozenLake-v1',
                                         desc=generate_random_map(N), is_slippery=False, render_mode="rgb_array")
@@ -50,34 +50,75 @@ def depth_first_branch_and_bound(env: Env[Discrete, Discrete], x: int) -> Tuple[
     return u, moves
 
 
-moves: list[int] = []
-times: list[float] = []
-for _ in range(5):
-    x, _ = env.reset()
-    print("Starting from state:", x)
-    start = time.time()
-    try:
-        u, moves = depth_first_branch_and_bound(env, cast(int, x))
-    except FunctionTimedOut:
-        print("Timed out...")
-        continue
-    stop = time.time()
-    times.append(stop - start)
-    print("Time taken:", times[-1])
-    print("Distance to goal:", u)
+@func_set_timeout(Timeout)
+def iterative_deepening_a_star(env: Env[Discrete, Discrete], x: int) -> Tuple[float, list[int]]:
+    stack: list[Tuple[int, int, list[int]]] = [(0, x, [])]
 
-plt.plot(times)
-plt.xlabel('Run')
-plt.ylabel('Time taken (s)')
-plt.title(f"Depth First Branch and Bound on Frozen Lake {N}x{N}")
-plt.savefig(f'frozen_lake_dfbnb_{N}x{N}.png')
+    u = 0
+    u_dash = 0
+    finished = False
 
-env.reset()
-frames: list = [env.render()]
-for action in moves:
-    _ = env.step(cast(Discrete, action))
-    frames.append(env.render())
+    # manhattan distance heuristic
+    h: Callable[[int], int] = lambda state: abs(
+        state // N - (N-1)) + abs(state % N - (N-1))
 
-imageio.mimsave(f"frozen_lake_dfbnb_{N}x{N}.gif", frames, duration=0.3)
+    while not finished:
+        u = u_dash
+        u_dash = float('inf')
+        stack = [(0, x, [])]
+        while stack:
+            depth, x, path = stack.pop()
+            for action in range(cast(int, env.action_space.n)):
+                transitions = env.unwrapped.P[x][action]
 
-print("GIF saved as frozen_lake_dfbnb.gif")
+                for _, y, _, _ in transitions:
+                    if depth + 1 + h(y) <= u and x != y:
+                        if y == N**2-1:
+                            moves = path + [action]
+                            finished = True
+                            break
+                        else:
+                            stack.append((depth + 1, y, path + [action]))
+                    if depth + 1 + h(y) < u_dash:
+                        u_dash = depth + 1 + h(y)
+    return u, moves
+
+
+def evaluate(env: Env[Discrete, Discrete], algorithm: Callable[[Env[Discrete, Discrete], int], Tuple[float, list[int]]]) -> None:
+    moves: list[int] = []
+    times: list[float] = []
+    for _ in range(5):
+        x, _ = env.reset()
+        print("Starting from state:", x)
+        start = time.time()
+        try:
+            u, moves = algorithm(env, cast(int, x))
+        except FunctionTimedOut:
+            print("Timed out...")
+            continue
+        stop = time.time()
+        times.append(stop - start)
+        print("Time taken:", times[-1])
+        print("Distance to goal:", u)
+
+    plt.plot(times)
+    plt.xlabel('Run')
+    plt.ylabel('Time taken (s)')
+    plt.title(f"{algorithm.__name__} on Frozen Lake {N}x{N}")
+    plt.savefig(f'frozen_lake_{algorithm.__name__}_{N}x{N}.png')
+
+    env.reset()
+    frames: list = [env.render()]
+    for action in moves:
+        _ = env.step(cast(Discrete, action))
+        frames.append(env.render())
+
+    imageio.mimsave(
+        f"frozen_lake_{algorithm.__name__}_{N}x{N}.gif", frames, duration=0.3)
+
+    print(f"GIF saved as frozen_lake_{algorithm.__name__}_{N}x{N}.gif")
+
+
+if __name__ == "__main__":
+    evaluate(env, depth_first_branch_and_bound)
+    evaluate(env, iterative_deepening_a_star)
